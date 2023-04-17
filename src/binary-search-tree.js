3cf3190d12a517afa6674b1482fe1566368c40e6
zubaryk <annazubaryk@tut.by>
2023-04-17 20:53:25 +0300

    feat: changes

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,16 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
+module.exports = {
+  Node
+};
 
 /**
 * Implement simple binary search tree according to task description
